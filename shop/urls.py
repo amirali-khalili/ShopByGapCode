526c600c648081d0ba4060007be0c846022e9600
amirali <167849011+amirali-khalili@users.noreply.github.com>
2026-04-24 17:12:12 +0330

    Add Pages

--- shop/urls.py
+++ shop/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('order/<int:order_id>/', views.order_detail, name='order_detail'),
     path('profile/', views.user_profile, name='profile'),
+    path('projects/', views.projects, name='projects'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
